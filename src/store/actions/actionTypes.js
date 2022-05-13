672f4e9db2341c2f9ee3f91d029d43a9938f9907
Sadab Hafiz <sadabhafizny.com>
2022-05-13 00:26:27 -0400

    Updated campus reducers to add edit functionality

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -2,15 +2,14 @@
 
 //All campuses
 export const FETCH_ALL_CAMPUSES = "FETCH_ALL_CAMPUSES";
+export const ADD_CAMPUS= "ADD_CAMPUS";
+export const DELETE_CAMPUS= "DELETE_CAMPUS";
+export const EDIT_CAMPUS = "EDIT_CAMPUS";
 
 //Single campus
 export const FETCH_CAMPUS = "FETCH_CAMPUS";
 
-//Delete campus
-export const DELETE_CAMPUS= "DELETE_CAMPUS";
 
-//Add campus
-export const ADD_CAMPUS= "ADD_CAMPUS";
 
 //All students
 export const FETCH_ALL_STUDENTS = "FETCH_ALL_STUDENTS";
